05189e8ec669c097867a5fa0c90c7eecf1f5dbdd
himanushi <himanushi@users.noreply.github.com>
2021-04-08 11:10:06 +0900

    Update app.svelte

--- src/app.svelte
+++ src/app.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-  import "./tailwind-styles.svelte";
+import "./tailwind-styles.svelte";
 
-  const name: string = "world";
+const name: string = "world";
 </script>
 
-<h1 class="hello">Hello {name}!</h1>
+<h1>Hello {name}!</h1>
 
 <style lang="scss">
-  .hello {
-    @apply text-center;
-  }
+h1 {
+  @apply text-center;
+}
 </style>
